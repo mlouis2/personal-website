346dfa368aba65d36fb733df18e459d037b4c00c
Maddie Louis <mlouis2@lion.lmu.edu>
2020-05-15 16:52:18 -0700

    finished welcome page

--- src/content/Welcome.js
+++ src/content/Welcome.js
@@ -3,7 +3,25 @@ import React from 'react';
 import '../styles/Welcome.css';
 
 function Welcome() {
-    return <div>hi there</div>;
+    return (
+        <div id="welcome">
+            <div id="intro">
+                <div id="photo">
+                    <img src={require('../images/profile.jpg')}></img>
+                </div>
+                <div id="card">
+                    <div id="cardTitle">
+                        Hi there!
+                    </div>
+                    <br />
+                    <p>
+                        My name is Maddie Louis and I am a rising senior computer science major at Loyola
+                        Marymount University. Click around my website to learn more about me and my work!
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Welcome;
