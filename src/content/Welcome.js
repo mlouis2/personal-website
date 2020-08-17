816b5c93487bebde0885474a17a1159b6bbd0a85
Maddie Louis <mlouis2@lion.lmu.edu>
2020-08-17 14:07:47 -0700

    Connecting GitHub to new Macbook
    Logging into GitHub on new computer

--- src/content/Welcome.js
+++ src/content/Welcome.js
@@ -15,8 +15,9 @@ function Welcome() {
                     </div>
                     <br />
                     <p>
-                        My name is Maddie Louis and I am a rising senior computer science major at Loyola
-                        Marymount University in Los Angeles, California. Click around my website to learn more about me and my work!
+                        My name is Maddie Louis and I'm a senior computer science major at Loyola Marymount University (LMU), currently
+                        seeking full-time opportunities in the software engineering field. Click around my website to learn more about 
+                        my work experience, side projects, and involvement at LMU. 
                     </p>
                 </div>
             </div>
